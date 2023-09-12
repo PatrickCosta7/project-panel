b7bbc56749b7f2c5aad6263e7947e23ea91506ac
Patrick Costa <pl.costa@hotmail.com>
2023-09-12 13:07:35 -0300

    Update 01 - 12092023

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,20 +3,22 @@ import styles from "./Card.module.scss"
 export default function Card(props) {
     return (
 
-        <div className={styles.card}>
-            <h5 className={styles.card__titulo}>{props.titulo}</h5>
-            <img className={styles.card__imagem} src={props.imagem} alt="Imagem do Card" />
-            <ul className={styles.card__lista}>
-                <li>
-                    <p>{props.descricao}</p>
-                </li>
-                <li>{props.visto}</li>
-                <li>{props.curtido}</li>
-                <li>
-                    <button>Modificar</button>
-                    <button>Apagar</button>
-                </li>
-            </ul>
+        <div className={`${styles.col_xs_6} ${styles.col_md_3} ${styles.col_lg_2}`}>
+            <div className={styles.card}>
+                <h5 className={styles.card__titulo}>{props.titulo}</h5>
+                <img className={styles.card__imagem} src={props.imagem} alt="Imagem do Card" />
+                <ul className={styles.card__lista}>
+                    <li>
+                        <p>{props.descricao}</p>
+                    </li>
+                    <li>{props.visto}</li>
+                    <li>{props.curtido}</li>
+                    <li>
+                        <button>Modificar</button>
+                        <button>Apagar</button>
+                    </li>
+                </ul>
+            </div>
         </div>
 
     )
--- src/pages/Todos/index.jsx
+++ src/pages/Todos/index.jsx
@@ -4,20 +4,18 @@ import styles from "./Todos.module.scss"
 export default function Todos() {
     return (
         <>
-            <section className={styles.container}>
+            <section className={styles.row}>
 
-                <Card 
+                <Card
                     titulo="Your name"
                     imagem="https://br.web.img3.acsta.net/pictures/17/10/04/19/01/4966397.jpg"
                     descricao="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eveniet, autem magnam adipisci, facilis quaerat nisi harum cumque quibusdam deserunt dolorem eligendi accusamus!"
                     visto="Assistido"
                     curtido="Curtido"
                 />
-
                 
-
             </section>
-        
+
         </>
     )
 }
